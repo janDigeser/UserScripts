6e2cabe5d9741d4331154ed59b822bb269146daa
Jan Digeser <22981726+janDigeser@users.noreply.github.com>
2026-04-20 16:53:13 +0200

    change match URL
    Fixes issue when loading the page on different url and then switching to the attendace screen.

--- kenjoGleitzeit.user.js
+++ kenjoGleitzeit.user.js
@@ -3,8 +3,8 @@
 // @namespace    de.rus1rius
 // @updateURL    https://github.com/janDigeser/UserScripts/raw/refs/heads/main/kenjoGleitzeit.user.js
 // @license      MIT
-// @version      1.4
-// @match        https://app.kenjo.io/cloud/attendance/my-attendance
+// @version      1.5
+// @match        https://app.kenjo.io/*
 // ==/UserScript==
 (function () {
     'use strict';
